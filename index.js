ccbe4f7bade30faca48c243932c9c9bc7b311037
Camper97 <ysonson1997@yahoo.com>
2021-11-26 21:41:53 +0200

    Adjusting resposes

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Zanaty World!");
+});
+
+app.get("/game", (req, res) => {
+  res.send("Zanaty Game!");
 });
 
 app.listen(port, () => {
